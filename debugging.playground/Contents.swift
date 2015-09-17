2b5978d398cea53ef40b3630b31d0ab155c6940c
Olivia Gu <oliviayugu@berkeley.edu>
2015-09-16 23:46:16 -0700

    HW 1 Done!

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,41 +15,41 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]!
+        wordB = words[1]!
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q1 HERE]
+//: String! need to correspond to words[0]!, since we are forcibly unwrapping and need to make sure values not nil
     
 
     
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
+    static func arePalindromes(words: [String]) -> Bool! {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+//: let --> constant. Can't increment constant! so had to change let i to var i. also changed function to static type since it doesn't require an instance to be created to invoke the method
     
     
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
-        var lenA = wordA.characters.count
-        var lenB = wordB.characters.count
+    static func isAnagram(wordA: String, wordB: String) -> Bool? {
+        var countLetters = [Character : Int]()
+        let lenA = wordA.characters.count
+        let lenB = wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,17 +75,17 @@ class Foo {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
+//: Initialization of dictionary was wrong. also changed var lenA and var lenB to 'let's! changed final return to be true and made it a static :)
 
 
 //: **Do not** change anything below.
